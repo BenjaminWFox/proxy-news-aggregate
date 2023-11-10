7433af6d5baf2123233e83d74a32d1a4c7a776e6
Ben Fox <ben.fox@salesforce.com>
2023-11-09 16:26:03 -0800

    Add GET endpoint

---
+++ src/pages/api/primitives.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log('Endpoint called: /text');
+
+  if (req.method !== 'GET') {
+    res.status(405).json({ message: 'Method Not Allowed'});
+    return;
+  }
+
+  try {
+    const { str, num, bool } = req.query;
+
+    if (!str && !num && !bool) {
+      res.status(500).json({ message: `You must provide at least one query parameter "str|num|bool"`});
+      console.log('500 - no parameters');
+      return;
+    }
+
+    if (num && !Number(num)) {
+      res.status(500).json({ message: "`num` must be a number"});
+      console.log('500 - num was not a number');
+      return;
+    }
+
+    if ((bool as string).toLowerCase() !== 'false' && (bool as string).toLowerCase() !== 'true') {
+      res.status(500).json({ message: "`bool` must be either 'true' or 'false'"});
+      console.log('500 - bool not true or false');
+
+      return;
+    }
+
+    console.log(' - ', str, num, bool);
+
+    res.status(200).json({ message: `You provided this query; str: ${str}, num: ${num}, bool: ${bool}`, data: { str, num: Number(num), bool: Boolean(bool)}});
+  } catch (e) {
+    res.status(500).json({ message: `There was an error: ${JSON.stringify(e)}`});
+  }
+}
